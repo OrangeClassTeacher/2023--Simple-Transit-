f45c1ee2936f68fcc0fa1c56b14d693f35b72dcb
khuderbaatar Tamir <khuderbaatartamir@khuderbaatars-MacBook-Pro.local>
2023-04-13 12:08:12 +0800

    user controller added

--- dist/index.js
+++ dist/index.js
@@ -7,10 +7,8 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const mongoose_1 = __importDefault(require("mongoose"));
+const user_router_1 = __importDefault(require("./routers/user.router"));
 const busstop_router_1 = __importDefault(require("./routers/busstop.router"));
-// const cors = require("cors");
-// const dotenv = require("dotenv")
-// const busRoutes = require("./routers/busstop.router")
 dotenv_1.default.config();
 const url = process.env.MONGO_DB_URI || "";
 mongoose_1.default
@@ -22,6 +20,7 @@ const port = process.env.PORT;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use("/api", busstop_router_1.default);
+app.use("/api", user_router_1.default);
 app.get("/api", (req, res) => {
     res.json({ message: "Success" });
 });
