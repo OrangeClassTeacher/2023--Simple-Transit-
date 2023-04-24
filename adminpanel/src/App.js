74015a26d799bf528d825482589a5785668ad033
ochko0421 <ochko0421@gmail.com>
2023-04-24 09:22:05 +0800

    little fix on admin

--- adminpanel/src/App.js
+++ adminpanel/src/App.js
@@ -1,77 +1,101 @@
-import './App.css';
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+import { GoogleMap, Marker, useLoadScript, Polyline } from '@react-google-maps/api';
 
 
 function App() {
-
-  const [data, setData] = useState([])
-  const [busStop, setBusStop] = useState([])
-  const [route,setRoute]=useState([])
+  const [data, setData] = useState([]);
+  const [selectedBusStops, setSelectedBusStops] = useState([]);
+  const [routes, setRoutes] = useState([]);
+  const [route, setRoute] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:9000/api/busstops")
-      .then(res => setData(res.data.result))
-  }, [])
-
+    axios
+      .get("http://localhost:9000/api/busstops")
+      .then((res) => setData(res.data.result));
+  }, []);
 
   function onSelect(e) {
-   
-    setBusStop([...busStop, e.target.value])
-    
-    console.log([...busStop, e.target.value]);
+    setSelectedBusStops([...selectedBusStops, e.target.value]);
+
 
   }
 
-  function addRoute(){
-    axios.get("http://localhost:9000/api/busstops")
-    .then(res=> setRoute(res.data.result))
-    console.log(route);
-    setRoute(route.filter((e)=> busStop[0]!==e.busStopName
-    ))
-    console.log(route);
+  function addRoute() {
+    axios.get("http://localhost:9000/api/busstops").then((res) => {
+      const allStops = res.data.result;
+      const filteredStops = allStops.filter((route) =>
+        selectedBusStops.includes(route.busStopName)
+      );
+      setRoutes(filteredStops);
+
+      console.log(routes);
+    });
   }
 
-
-  function onAdd(){
-    axios.post("http://localhost:9000/api/busroutes/create",{
-      busRouteName:"",
-      busStopDetails:route,
-      busRouteId:""
-    })
-    .then(res=>{
-      console.log(res);
-    })
+  function onAdd() {
+    axios
+      .post("http://localhost:9000/api/busroutes/create", {
+        busRouteName: "",
+        busStopDetails: routes,
+        busRouteId: "",
+      })
+      .then((res) => {
+        console.log(res);
+      });
   }
+
+  // const libraries = ['places'];
+
+  const mapContainerStyle = {
+    height: '400px',
+    width: '100%',
+  };
+
+  const center = {
+    lat: 47.90771,
+    lng: 106.88324,
+  };
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyAhjl1X_pQkIAeTUWlWv4cKKUDqgyxDCQE'
+  });
+  if (loadError) return 'Error loading maps';
+  if (!isLoaded) return 'Loading Maps';
+
+
+
   return (
     <div className="App">
-      <input placeholder='Чиглэлийн дугаар'/>
-      <input placeholder='Чиглэлийн нэр'/>
-      Чиглэлийн буудлуудыг сонгоx 
+      <input placeholder="Чиглэлийн дугаар" />
+      <input placeholder="Чиглэлийн нэр" />
+      Чиглэлийн буудлуудыг сонгоx
       <select onChange={(e) => onSelect(e)}>
-        {
-          data.map((e, i) => {
-            return <option value={e.busStopName} key={i}>
+        {data.map((e, i) => {
+          return (
+            <option value={e.busStopName} key={i}>
               {e.busStopName}
-
             </option>
-
-          })
-        }
-      </select> 
-      <ul> 
-
-        {
- busStop.map((e, i) => {
-  return <li key={i}>{(e)}</li>
-}) 
-        }
-         
-
-
+          );
+        })}
+      </select>
+      <ul>
+        {selectedBusStops.map((e, i) => {
+          return <li key={i}>{e}</li>;
+        })}
       </ul>
-        <button onClick={()=>addRoute()}>Чиглэл шалгаx</button>
-      <button>Чиглэлийг нэмэx</button>
+      <button onClick={() => addRoute()}>Чиглэл шалгаx</button>
+      <button onClick={() => onAdd()}>Чиглэлийг нэмэx</button>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={15}
+        center={center}
+      >
+        {route.length > 0 && (
+          <Polyline path={route} options={{ strokeColor: '#FF0000' }} />
+        )}
+      </GoogleMap>
     </div>
   );
 }
