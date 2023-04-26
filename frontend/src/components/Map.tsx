1dd5068fa74d3a0f15128a0ab5d389e0ee0473ce
ochirsukhM <orange.22.ochirsukhmunkhchuluun@gmail.com>
2023-04-26 10:29:02 +0800

    component folder added and env file

---
+++ frontend/src/components/Map.tsx
@@ -0,0 +1,31 @@
+import { GoogleMap ,useLoadScript} from "@react-google-maps/api";
+import React from "react";
+
+
+export default function Map(){
+    const mapContainerStyle = {
+        height: '400px',
+        width: '100%',
+      };
+
+      const center = {
+        lat: 47.90771,
+        lng: 106.88324,
+      };
+      const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: process.env.NEXT_GOOGLE_API_KEY as string
+      });
+      if (loadError) return 'Error loading maps';
+      if (!isLoaded) return 'Loading Maps';
+
+
+      return (<div>
+
+            <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                center={center}
+                zoom={14}
+            />
+ 
+      </div>)
+}
