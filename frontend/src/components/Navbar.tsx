ded04645b2cc21cb041072edf41edb390c97edcd
khuderbaatar Tamir <khuderbaatartamir@khuderbaatars-MacBook-Pro.local>
2023-04-27 10:44:35 +0800

    some pages added

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Link from 'next/link'
+
 
 interface Props { }
 
@@ -6,27 +8,28 @@ export const Navbar = (): JSX.Element => {
     return (
         <nav className="navbar1">
             <div className="navbar-logo">
-                <a href="#">
+                <Link href="/page1">
                     <img src="logo.png" alt="Logo" width={50} height={50} />
-                </a>
+                </Link>
             </div>
-            <div className="navbar-buttons">
-                <a href="#" className="text-gray-600">
+            <div className="navbar-buttons ">
+                <Link href="/page1" className="text-gray-600 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 rounded">
                     BUS
-                </a>
-                <a href="#" className="text-gray-600">
+                </Link>
+                <Link href="/ZenlyPage" className="text-gray-600">
                     TRACKING
-                </a>
-                <a href="#" className="text-gray-600">
+                </Link>
+                <Link href="/LightPage" className="text-gray-600">
                     TRAFFIC LIGHT
-                </a>
+                </Link>
             </div>
             <div className="navbar-profile">
-                <a href="#">
+                <Link href="#">
                     <img src="icon.png" alt="Profile Icon" width={50} height={50} />
-                </a>
+                </Link>
             </div>
         </nav>
+
     );
 };
 
