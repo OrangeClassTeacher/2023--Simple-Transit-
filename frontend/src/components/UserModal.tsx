db5a7e101afaaeedbba0d6988a7977c3b46c2fd2
khuderbaatar Tamir <khuderbaatartamir@khuderbaatars-MacBook-Pro.local>
2023-05-01 15:15:53 +0800

    userModal style Updated

--- frontend/src/components/UserModal.tsx
+++ frontend/src/components/UserModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Link from "next/link"
 
 // export interface Modal {
 //     modal: boolean
@@ -8,19 +9,44 @@ import Navbar from './Navbar'
 export default function UserModal({ modal }: { modal: boolean }): JSX.Element {
     const dn = modal ? "block" : "none"
     return (
-        <div className='rounded-sm w-3/12 bg-white'
-            style={{ display: dn }}
-        >
-            <div className='h-full flex-col accSettings'>
-                <img src="icon.png" alt="profile image" width={60} height={60} className="self-center" />
-                <p className='text-center text-xl'>Tamir Khuderbaatar</p>
-                <p className='text-center text-xs'>htamir80@gmail.com</p>
-                <p className='drop-shadow-md text-black'>Chat</p>
-                <p className='drop-shadow-md text-black'>Friends</p>
-                <p className='drop-shadow-md text-black'>Privacy and Policy</p>
-                <p className='drop-shadow-md text-black'>Settings</p>
-                <p className='bg-blue-600 w-30 text-center rounded-xl text-white'>+Add account</p>
-                <p className='bg-red-600 w-20 text-center rounded-xl text-white'>Log Out</p>
+
+        <div id='dropdownModal' className="mx-auto right-0 mt-2 w-60 z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-60 dark:bg-gray-700 dark:divide-gray-600">
+            <div className="bg-white rounded overflow-hidden shadow-lg">
+                <div className="text-center p-6 bg-gray-800 border-b">
+                    <img src="profileImage.png" alt="Profile Icon" className="rounded-full object-cover h-full settingPro mx-auto" width={50} height={50} />
+                    <p className="pt-2 text-lg font-semibold text-gray-50">Tamir Khuderbaatar</p>
+                    <p className="text-sm text-gray-100">htamir80@gmail.com</p>
+                    <div className="mt-5">
+                        <a className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100 cursor-pointer">
+                            Manage your Account
+                        </a>
+                    </div>
+                </div>
+                <div className='border-b'>
+                    <ul>
+                        <li>
+                            <a href="#" className='px-4 py-2 hover:bg-gray-100 flex'>Chat</a>
+                        </li>
+                        <li>
+                            <a href="#" className='px-4 py-2 hover:bg-gray-100 flex'>Friends</a>
+                        </li>
+                        <li>
+                            <a href="#" className='px-4 py-2 hover:bg-gray-100 flex'>Privace and Policy</a>
+                        </li>
+                        <li>
+                            <a href="#" className='px-4 py-2 hover:bg-gray-100 flex'>Help center</a>
+                        </li>
+                        <li>
+                            <a href="#" className='px-4 py-2 hover:bg-gray-100 flex'>Settings</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="">
+                    <button className="w-full px-4 py-2 pb-4 bg-red-700 hover:bg-red-500 flex">
+                        <p className="text-lg font-medium text-white leading-none mx-auto ps-auto"> Logout
+                        </p>
+                    </button>
+                </div>
             </div>
         </div>
     )
